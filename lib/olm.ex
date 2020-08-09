e3004594792c9d28706e8eec07af842eded30c6c
Niklas Long <niklas@long.ch>
2020-08-09 14:22:47 +0200

    Improves error handling for unloaded NIFs
    Replaces raise with :erlang.nif_error.

--- lib/olm.ex
+++ lib/olm.ex
@@ -5,11 +5,9 @@ defmodule Olm do
 
   @on_load :load_nifs
 
-  def load_nifs do
-    :erlang.load_nif('priv/native/olm_nifs', 0)
-  end
+  def load_nifs(), do: :erlang.load_nif('priv/native/olm_nifs', 0)
 
-  def version do
-    raise "NIF version/0 not implemented"
-  end
+  def version(), do: error("version/0")
+
+  defp error(function_name), do: :erlang.nif_error("NIF #{function_name} not implemented")
 end
