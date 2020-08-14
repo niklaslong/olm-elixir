313f41069995051e0e98044cc9b24260ba4b80ff
Niklas Long <niklas@long.ch>
2020-08-14 10:27:53 +0200

    Set indent to 4

--- native/src/olm_nifs.c
+++ native/src/olm_nifs.c
@@ -8,46 +8,46 @@
 static ERL_NIF_TERM
 version(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  uint8_t major, minor, patch;
-  olm_get_library_version(&major, &minor, &patch);
+    uint8_t major, minor, patch;
+    olm_get_library_version(&major, &minor, &patch);
 
-  return enif_make_tuple3(env,
-                          enif_make_uint(env, major),
-                          enif_make_uint(env, minor),
-                          enif_make_uint(env, patch));
+    return enif_make_tuple3(env,
+                            enif_make_uint(env, major),
+                            enif_make_uint(env, minor),
+                            enif_make_uint(env, patch));
 }
 
 static ERL_NIF_TERM
 account_size(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  size_t size = olm_account_size();
+    size_t size = olm_account_size();
 
-  return enif_make_ulong(env, size);
+    return enif_make_ulong(env, size);
 }
 
 static ERL_NIF_TERM
 session_size(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  size_t size = olm_session_size();
+    size_t size = olm_session_size();
 
-  return enif_make_ulong(env, size);
+    return enif_make_ulong(env, size);
 }
 
 static ERL_NIF_TERM
 utility_size(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
-  size_t size = olm_utility_size();
+    size_t size = olm_utility_size();
 
-  return enif_make_ulong(env, size);
+    return enif_make_ulong(env, size);
 }
 
 // Let's define the array of ErlNifFunc beforehand:
 static ErlNifFunc nif_funcs[] = {
-  // {erl_function_name, erl_function_arity, c_function}
-  { "version", 0, version },
-  { "account_size", 0, account_size },
-  { "session_size", 0, session_size },
-  { "utility_size", 0, utility_size }
+    // {erl_function_name, erl_function_arity, c_function}
+    { "version", 0, version },
+    { "account_size", 0, account_size },
+    { "session_size", 0, session_size },
+    { "utility_size", 0, utility_size }
 };
 
 ERL_NIF_INIT(Elixir.Olm, nif_funcs, NULL, NULL, NULL, NULL)
