96159515a94bfb30585b3c76f1a3ca916024cebd
Niklas Long <niklas@long.ch>
2020-09-05 11:56:42 +0200

    Implement verification on Utility module

--- lib/olm/utility.ex
+++ lib/olm/utility.ex
@@ -11,4 +11,13 @@ defmodule Olm.Utility do
       {:error, error} -> raise NIFError, error
     end
   end
+
+  def verify_ed25519(key, message, signature)
+      when is_binary(key) and is_binary(message) and is_binary(signature) do
+    case NIF.utility_ed25519_verify(key, message, signature) do
+      {:ok, _} -> {:ok, "verified"}
+      {:error, 'BAD_MESSAGE_MAC'} -> {:error, "bad message MAC"}
+      {:error, error} -> raise NIFError, error
+    end
+  end
 end
--- test/olm/utility_test.exs
+++ test/olm/utility_test.exs
@@ -1,12 +1,34 @@
 defmodule Olm.UtilityTest do
   use ExUnit.Case
-  alias Olm.Utility
+  alias Olm.{Utility, Account}
 
   doctest Utility
+
+  defp create_account(_context), do: %{account: Account.create()}
+  defp identity_keys(context), do: %{identity_keys: Account.identity_keys(context.account)}
+  defp sign(context), do: %{signature: Account.sign(context.account, "test")}
 
   describe "sha256/1:" do
     test "returns a hash of the input string" do
       assert "input" |> Utility.sha256() |> is_binary
+    end
+  end
+
+  describe "verify_ed25519/3:" do
+    setup [:create_account, :identity_keys, :sign]
+
+    test "verifies signature", context do
+      {:ok, msg} =
+        Utility.verify_ed25519(context.identity_keys.ed25519, "test", context.signature)
+
+      assert msg == "verified"
+    end
+
+    test "returns error for bad signature", context do
+      {:error, msg} =
+        Utility.verify_ed25519(context.identity_keys.ed25519, "bad_msg", context.signature)
+
+      assert msg == "bad message MAC"
     end
   end
 end
